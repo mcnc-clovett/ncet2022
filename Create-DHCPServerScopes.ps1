e5784a864254ca7d93ee2ad508829fe03c46d838
Chris Lovett <clovett-adm@ad.example.edu>
2022-03-31 01:08:59 -0400

    Added DHCP Server Stuff

---
+++ Create-DHCPServerScopes.ps1
@@ -0,0 +1,23 @@
+﻿$server = 'netserv1'
+$scopes = Import-Csv .\DHCP_Scopes.csv
+
+Set-DhcpServerv4OptionValue `
+    -ComputerName $server `
+    -DnsDomain 'ad.example.edu' `
+    -DnsServer 192.0.2.5
+
+foreach ( $s in $scopes ) {
+    $newScope = Add-DhcpServerv4Scope -ComputerName $server `
+        -StartRange $s.StartRange `
+        -EndRange $s.EndRange `
+        -Name $s.Name `
+        -State $s.State `
+        -LeaseDuration $s.LeaseDuration `
+        -SubnetMask $s.SubnetMask `
+        -Type $s.Type `
+        -PassThru
+
+    Set-DhcpServerv4OptionValue -ComputerName $server `
+        -ScopeId $newScope.ScopeId `
+        -Router $s.Router
+}
